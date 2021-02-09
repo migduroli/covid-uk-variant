1c2241403ac69e11f893dece6f11358906cd7e99
Miguel A. Duran-Olivencia <migduroli@gmail.com>
2021-02-09 19:32:10 +0100

    feat: add compute_error function with propagation of uncertainties feat: add confidence intervals to graph

--- toolbox/model.py
+++ toolbox/model.py
@@ -54,6 +54,7 @@ def control(t: Union[float, np.array],
 
 # endregion
 
+
 # region Aux:
 
 class SirModelType(Enum):
@@ -66,6 +67,7 @@ class PredictionTarget(Enum):
     NewCases = 2
 
 # endregion
+
 
 # region SIR model
 
@@ -88,20 +90,20 @@ class SirModels:
     def __str__(self):
         return f"\n" \
             f"SirModel type: {self.type}\n" \
-            f"--------------------------\n" \
+            f"----------------------------------------------------\n" \
             f"N: {self.total_population}\n" \
-            f"beta: {self.beta}\n" \
-            f"alpha: {self.alpha}\n" \
-            f"--------------------------\n" \
-            f"R0: {self.beta/self.alpha}\n" \
+            f"beta: {self.beta} +/- {self.beta_err}\n" \
+            f"alpha: {self.alpha} +/- {self.alpha_err}\n" \
+            f"----------------------------------------------------\n" \
+            f"R0: {self.beta/self.alpha} +/- ({(self.beta_err/self.beta + self.alpha_err/self.alpha) * self.beta/self.alpha})\n" \
             f"PM times: {self.measure_init}\n" \
-            f"PM scales: {self.scales}\n" \
+            f"PM scales: {self.scales} +/- {self.scales_err}\n" \
             f"Social delay: {self.delay_time}\n" \
             f"Social inertia: {self.inertial_time}\n" \
-            f"--------------------------\n" \
+            f"----------------------------------------------------\n" \
             f"Vaccination rate: {self.vaccination_rate}\n" \
             f"Vaccination begins: {self.t_vaccination}\n" \
-            f"--------------------------\n"
+            f"----------------------------------------------------\n"
 
     def _initialise(
             self, n_total: float, n_infected: float, n_recovered: float,
@@ -110,9 +112,18 @@ class SirModels:
     ):
         self.beta = None
         self.alpha = None
+        #
+        self.beta_err = None
+        self.alpha_err = None
+        #
         self.measure_init = None
+        #
         self.maturities = None
+        self.maturities_err = None
+        #
         self.scales = None
+        self.scales_err = None
+        #
         self.initial_guess = None
         self.bounds = None
         self.delay_time = None
@@ -259,14 +270,22 @@ class SirModels:
             bounds=self.bounds
         )
 
+        p_err = np.sqrt(np.diag(p_cov))
+
+        idx_off = 2
         if self.type == SirModelType.Free:
             self.beta, self.alpha = p_opt
+            self.beta_err, self.alpha_err = p_err[0: idx_off]
 
         elif self.type == SirModelType.Controlled:
-            idx_off = 2
             self.beta, self.alpha = p_opt[0: idx_off]
+            self.beta_err, self.alpha_err = p_err[0: idx_off]
+
             self.maturities = p_opt[idx_off:idx_off + self.n_peaks].tolist()
+            self.maturities_err = p_err[idx_off:idx_off + self.n_peaks].tolist()
+
             self.scales = p_opt[idx_off + self.n_peaks: idx_off + 2 * self.n_peaks].tolist()
+            self.scales_err = p_err[idx_off + self.n_peaks: idx_off + 2 * self.n_peaks].tolist()
 
         return [p_opt, p_cov]
 
@@ -309,7 +328,40 @@ class SirModels:
             linewidth=1
         )
 
-    def _add_control(self, ax, x_data):
+    def _compute_error_from_trajectory(self, s, i, r):
+        return np.sqrt(
+            np.square(s * i * self.beta_err / self.total_population)
+            + 2 * np.sqrt(i * self.alpha_err)
+        )
+
+    def _get_confidence_intervals(self, dt):
+        z = self._simulate(
+            time=dt,
+            beta=self.beta,
+            alpha=self.alpha,
+            maturities=self.maturities,
+            scales=self.scales
+        )
+
+        sigma = self._compute_error_from_trajectory(s=z[:, 0], i=z[:, 1], r=z[:, 2])
+        return [3 * sigma, - 3 * sigma]
+
+    def add_confidence_intervals(self, dt, z, ax, time_offset=None, limits=None):
+        conf_interval = self._get_confidence_intervals(dt)
+
+        t0 = time_offset if time_offset else 0
+        dt = dt if not limits else dt[limits[0]:limits[1]]
+        z = z if not limits else z[limits[0]:limits[1]]
+        err_max = conf_interval[0] \
+            if not limits else conf_interval[0][limits[0]:limits[1]]
+        err_min = conf_interval[1] \
+            if not limits else conf_interval[1][limits[0]:limits[1]]
+
+        ax.fill_between(
+            t0 + dt, z+err_min, z+err_max, alpha=0.15, color='g'
+        )
+
+    def add_control(self, ax, x_data):
         if not (not self.measure_init):
             policy = control(
                 t=x_data,
@@ -329,7 +381,7 @@ class SirModels:
             fig, ax = plt.subplots()
         self._add_actual_data(ax, x_data, y_data)
         self._add_prediction(ax, x_data)
-        self._add_control(ax, x_data)
+        self.add_control(ax, x_data)
         fig.tight_layout()
         if not (not x_lims):
             ax.set_xlim(x_lims)
