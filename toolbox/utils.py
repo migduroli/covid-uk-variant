9331ed812d4feb5755a1cafd5d4121baf8178bec
Miguel A. Duran-Olivencia <migduroli@gmail.com>
2021-02-09 19:32:59 +0100

    refact: make_plot split into smaller functions to better organise the code

--- toolbox/utils.py
+++ toolbox/utils.py
@@ -15,6 +15,127 @@ TIME_ARROW = [
 ]
 
 
+def setup_matplotlib(
+        font_family: str = 'serif',
+        font_size: int = 8,
+        fig_size: tuple = (5, 3),
+        fig_dpi: int = 100
+):
+    plt.rc('font', family=font_family)
+    plt.rc('font', size=font_size)
+    plt.rcParams['figure.figsize'] = fig_size
+    plt.rcParams['figure.dpi'] = fig_dpi
+    # plt.rc('text', usetex=True)
+
+
+def plot_init(
+        model: SirModels,
+        dt: np.array,
+        simulation,
+        x_axis: np.array,
+        x_train: np.array,
+        y_train: np.array,
+        train_idx: int,
+        x_total: np.array,
+        y_total: np.array,
+        total_idx: int,
+        x_ticks: np.array,
+        time_offset: int,
+):
+    fig, ax = plt.subplots()
+    ax.grid(b=True, which='major', color='#666666', linestyle='--', alpha=0.25, axis='x')
+    ax.set_xticks(x_axis[:-1])
+    ax.set_xticklabels(x_ticks, rotation=90)
+    ax.bar(time_offset + x_train, y_train, color='b', alpha=0.4)
+    ax.bar(time_offset + x_total[train_idx:], y_total[train_idx:], color='red', alpha=0.4)
+    ax.plot(time_offset + x_total, simulation[:total_idx + 1], '-', color='darkturquoise')
+    ax.plot(time_offset + dt[total_idx:], simulation[total_idx:], '--', color='darkturquoise')
+    model.add_confidence_intervals(dt=dt, z=simulation, ax=ax, time_offset=time_offset)
+    return fig, ax
+
+
+def setup_limits_and_ticks(axis, model_type: SirModelType, second_wave: bool, third_wave: bool):
+    if model_type == SirModelType.Free:
+        axis.set_ylim([0, 90_000])
+        axis.set_yticks(np.arange(0, 80_000, 25_000))
+    if model_type == SirModelType.Controlled and second_wave:
+        axis.set_ylim([0, 140_000])
+        axis.set_yticks(np.arange(0, 140_000, 30_000))
+    if model_type == SirModelType.Controlled and third_wave:
+        axis.set_ylim([0, 140_000])
+        axis.set_yticks(np.arange(0, 140_000, 30_000))
+
+
+def add_control_plot(axis, model: SirModels, model_type: SirModelType, dt: np.array, time_offset: int):
+    if model_type == SirModelType.Controlled:
+        model.add_control(axis, time_offset + dt)
+
+
+def add_inset_plot(
+        axis,
+        inset_plot: bool,
+        model: SirModels,
+        dt: np.array,
+        third_wave: bool,
+        x_axis: np.array,
+        y_total: np.array,
+        simulation: np.array,
+        train_idx: int,
+        total_idx: int,
+        time_offset: int,
+        x_ticks: np.array,
+):
+    if inset_plot:
+        ax_inset = plt.axes([0, 0, 1, 1])
+        ip = InsetPosition(axis, [0.02, 0.55, 0.4, 0.4])
+        ax_inset.set_axes_locator(ip)
+        ax_inset.yaxis.tick_right()
+        ax_inset.patch.set_alpha(0.7)
+
+        limits = None
+        color_style = None
+
+        if not third_wave:
+            color_style = 'blue'
+            limits = [0, int(0.75 * train_idx)]
+            ax_inset.set_xticks(x_axis[:8:2])
+            ax_inset.set_xticklabels(x_ticks[:8:2], rotation=90)  # plt.show()
+        if third_wave:
+            color_style = 'red'
+            limits = [train_idx, total_idx]
+            ax_inset.set_xticks(x_axis[10:-1:2])
+            ax_inset.set_xticklabels(x_ticks[10::2], rotation=90)  # plt.show()
+
+        ax_inset.bar(
+            time_offset + dt[limits[0]: limits[1]],
+            y_total[limits[0]: limits[1]],
+            color=color_style, alpha=0.4
+        )
+
+        ax_inset.plot(
+            time_offset + dt[limits[0]:limits[1]],
+            simulation[limits[0]: limits[1]],
+            '-', color='darkturquoise'
+        )
+
+        model.add_confidence_intervals(
+            dt=dt, z=simulation, ax=ax_inset, time_offset=time_offset, limits=limits
+        )
+
+
+def get_axis_data(df: pd.DataFrame, max_idx: int):
+    x_origin = pd.to_datetime(df.date.min(), format="%Y-%m-%d")
+    x_tick_idx = Months.List.Numbers.index(x_origin.month)
+    x_ticks = [f'{m}-2020' for m in Months.List.Names[x_tick_idx:]] + \
+              [f'{m}-2021' for m in Months.List.Names[0:max_idx]]
+
+    lengths = np.array(Months.List.Lengths(year=2020)[x_tick_idx:] +
+                       Months.List.Lengths(year=2021)[0:max_idx])
+    x_axis = [0] + lengths.cumsum().tolist()
+    x_offset = x_origin.day
+    return x_axis, x_ticks, x_offset
+
+
 def daily_tests_per_thousand(x):
     # The daily COVID tests has been steadily raising from
     # 0 -> 7 per thousand from Jan20 to Jan21.
@@ -129,83 +250,66 @@ def make_plot(
         third_wave: bool,
         inset_plot: bool = False
 ):
-    min_months_2020 = 1
-    max_months_2021 = 2
-    if third_wave:
-        min_months_2020 = 10
-        max_months_2021 = 8
+    max_months_2021 = 2 if not third_wave else 8
 
-    date_origin = pd.to_datetime(df.date.min(), format="%Y-%m-%d")
-    month_idx = Months.List.Numbers.index(date_origin.month)
-    months = [f'{m}-2020' for m in Months.List.Names[month_idx:]] + \
-             [f'{m}-2021' for m in Months.List.Names[0:max_months_2021]]
-
-    lengths = np.array(Months.List.Lengths(year=2020)[month_idx:] +
-                       Months.List.Lengths(year=2021)[0:max_months_2021])
-    x_axis = [0] + lengths.cumsum().tolist()
-
-    time_offset = date_origin.day
+    x_axis, x_ticks, x_offset = get_axis_data(
+        df=df,
+        max_idx=max_months_2021
+    )
 
     dt = np.arange(0, x_axis[-2])
+
     z = m.compute_trajectory(time=dt)
-
-    # plt.rc('text', usetex=True)
-    plt.rc('font', family='serif')
-    plt.rc('font', size=8)
-    plt.rcParams['figure.figsize'] = (5, 3)
-    plt.rcParams['figure.dpi'] = 100
-
-    fig, ax = plt.subplots()
-    ax.grid(b=True, which='major', color='#666666', linestyle='--', alpha=0.25, axis='x')
-    ax.set_xticks(x_axis[:-1])
-    ax.set_xticklabels(months, rotation=90)
 
     train_idx = x_total.tolist().index(x_train[-1])
     total_idx = dt.tolist().index(x_total[-1])
-    ax.bar(time_offset + x_train, y_train, color='b', alpha=0.4)
-    ax.bar(time_offset + x_total[train_idx:], y_total[train_idx:], color='red', alpha=0.4)
 
-    ax.plot(time_offset + x_total, z[:total_idx+1], '-', color='darkturquoise')
-    ax.plot(time_offset + dt[total_idx:], z[total_idx:], '--', color='darkturquoise')
+    setup_matplotlib()
 
-    if m_type == SirModelType.Free:
-        ax.set_ylim([0, 90_000])
-        ax.set_yticks(np.arange(0, 80_000, 25_000))
-    if m_type == SirModelType.Controlled and second_wave:
-        ax.set_ylim([0, 140_000])
-        ax.set_yticks(np.arange(0, 140_000, 30_000))
-    if m_type == SirModelType.Controlled and third_wave:
-        ax.set_ylim([0, 140_000])
-        ax.set_yticks(np.arange(0, 140_000, 30_000))
+    fig, ax = plot_init(
+        model=m,
+        dt=dt,
+        simulation=z,
+        x_axis=x_axis,
+        x_train=x_train,
+        y_train=y_train,
+        train_idx=train_idx,
+        x_total=x_total,
+        y_total=y_total,
+        total_idx=total_idx,
+        x_ticks=x_ticks,
+        time_offset=x_offset,
+    )
 
-    if m_type == SirModelType.Controlled:
-        m._add_control(ax, time_offset + dt)
+    setup_limits_and_ticks(
+        axis=ax,
+        model_type=m_type,
+        second_wave=second_wave,
+        third_wave=third_wave
+    )
 
-    if inset_plot:
-        ax_inset = plt.axes([0, 0, 1, 1])
-        ip = InsetPosition(ax, [0.02, 0.55, 0.4, 0.4])
-        ax_inset.set_axes_locator(ip)
-        ax_inset.yaxis.tick_right()
-        ax_inset.patch.set_alpha(0.7)
-        if not third_wave:
-            limits = [0, int(0.75 * train_idx)]
-            ax_inset.set_xticks(x_axis[:8:2])
-            ax_inset.set_xticklabels(months[:8:2], rotation=90)  # plt.show()
-            color_style = 'blue'
-        if third_wave:
-            color_style = 'red'
-            limits = [train_idx, total_idx]
-            ax_inset.set_xticks(x_axis[10:-1:2])
-            ax_inset.set_xticklabels(months[10::2], rotation=90)  # plt.show()
+    add_control_plot(
+        axis=ax,
+        model=m,
+        model_type=m_type,
+        dt=dt,
+        time_offset=x_offset
+    )
 
-        ax_inset.bar(
-            time_offset + dt[limits[0]: limits[1]],
-            y_total[limits[0]: limits[1]],
-            color=color_style, alpha=0.4)
-        ax_inset.plot(
-            time_offset + dt[limits[0]:limits[1]],
-            z[limits[0]: limits[1]],
-            '-', color='darkturquoise')
+    add_inset_plot(
+        axis=ax,
+        inset_plot=inset_plot,
+        model=m,
+        dt=dt,
+        third_wave=third_wave,
+        x_axis=x_axis,
+        y_total=y_total,
+        simulation=z,
+        train_idx=train_idx,
+        total_idx=total_idx,
+        time_offset=x_offset,
+        x_ticks=x_ticks,
+    )
 
     return fig, ax
 
